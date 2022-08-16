51ff3ddad921442b2445b820ad4de2976068e18b
tanhaokun <109639746+tanhaokun@users.noreply.github.com>
2022-08-16 14:02:23 +0800

    Update BookControllersecond.java

--- src/main/java/com/example/demo/BookControllersecond.java
+++ src/main/java/com/example/demo/BookControllersecond.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pen")
+@RequestMapping("/guojunjieshishei")
 public class BookControllersecond {
     @GetMapping
     public String getByid(){
-        System.out.println("333333");
-        return "3333333333";
+        System.out.println("郭俊杰是个傻逼");
+        return "郭俊杰是个傻逼";
     }
 }
